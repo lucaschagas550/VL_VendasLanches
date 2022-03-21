4da7fbc4e739c245bb90b218d11bdc62c629253c
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-03-21 14:15:40 -0300

    Feat: Alterando para usar Startup como na versão do .NET 5.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VL_VendasLanches.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Startup.cs
@@ -0,0 +1,45 @@
+﻿namespace VL_VendasLanches
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. 
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+             {
+                 endpoints.MapControllerRoute(
+                         name: "default",
+                         pattern: "{controller=Home}/{action=Index}/{id?}");
+             });
+        }
+    }
+}
