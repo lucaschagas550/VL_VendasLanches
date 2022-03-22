01544c14f456892b1a48f5d03f6a2bde70fc48bd
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-03-22 19:50:07 -0300

    Feat: Padrao repository para o model de lanches.

---
+++ Repositories/Interfaces/ILancheRepository.cs
@@ -0,0 +1,11 @@
+﻿using VL_VendasLanches.Models;
+
+namespace VL_VendasLanches.Repositories.Interfaces
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+        IEnumerable<Lanche> LanchesPreferidos { get; }
+        Lanche GetLancheById(int lancheId);
+    }
+}
---
+++ Repositories/LancheRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VL_VendasLanches.Context;
+using VL_VendasLanches.Models;
+using VL_VendasLanches.Repositories.Interfaces;
+
+namespace VL_VendasLanches.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LancheRepository(AppDbContext context)
+        {
+            _context=context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanche.Include(c => c.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanche
+                                                        .Where(l => l.IsLanchePreferido)
+                                                        .Include(c => c.Categoria);
+
+        public Lanche GetLancheById(int lancheId)
+        {
+            return _context.Lanche.FirstOrDefault(l => l.LancheId.Equals(lancheId));
+        }
+    }
+}
