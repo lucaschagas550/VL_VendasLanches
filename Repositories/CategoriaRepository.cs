bc20793723a589dafaf20868b27cee428e5e033a
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-03-22 19:36:11 -0300

    Feat: Padrao repository para o model categoria.

---
+++ Repositories/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using VL_VendasLanches.Context;
+using VL_VendasLanches.Models;
+using VL_VendasLanches.Repositories.Interfaces;
+
+namespace VL_VendasLanches.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context=context;
+        }
+
+        public IEnumerable<Categoria> Categorias => _context.Categoria;
+    }
+}
---
+++ Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+﻿using VL_VendasLanches.Models;
+
+namespace VL_VendasLanches.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
