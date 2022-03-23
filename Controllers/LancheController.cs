801c3b48291b79b39681ef9de5beb3ff8914c413
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-03-22 22:31:05 -0300

    Feat: Ajustando layout da exibição de lanches.

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VL_VendasLanches.Repositories.Interfaces;
+
+namespace VL_VendasLanches.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository=lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches); //enviando para view um IEnumerable de lanches
+        }
+    }
+}
