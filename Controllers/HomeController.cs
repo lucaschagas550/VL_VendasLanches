d20359af582a0868048fe1f91b178a4c36820c7d
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-03-25 20:20:46 -0300

    Feat: ViewComponent Feat: Lanches preferidos a serem exibidos na tela home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,20 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using VL_VendasLanches.Models;
+using VL_VendasLanches.Repositories.Interfaces;
+using VL_VendasLanches.ViewModels;
 
 namespace VL_VendasLanches.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ILancheRepository _lancheRepositorys;
+
+    public HomeController(ILancheRepository lancheRepositorys)
+    {
+        _lancheRepositorys=lancheRepositorys;
+    }
+
     public IActionResult Index()
     {
         #region
@@ -13,7 +22,12 @@ public class HomeController : Controller
         // TempData é um valor temporario que existe até ser recuperado, pode ser enviado para outra controller ou action, view para controller ou inverso
         #endregion
 
-        return View();
+        var homeViewModel = new HomeViewModel
+        {
+            LanchesPreferidos = _lancheRepositorys.LanchesPreferidos,
+        };
+
+        return View(homeViewModel);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using VL_VendasLanches.Models;
+
+namespace VL_VendasLanches.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Lanche> LanchesPreferidos { get; set; }
+    }
+}
