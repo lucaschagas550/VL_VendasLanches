563851ce888a7b09204cf3c95974eb34dd6324c9
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-03-26 14:58:02 -0300

    Feat: Filtro por categoria dos produtos

--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VL_VendasLanches.Models;
 using VL_VendasLanches.Repositories.Interfaces;
 using VL_VendasLanches.ViewModels;
 
@@ -13,7 +14,7 @@ namespace VL_VendasLanches.Controllers
             _lancheRepository=lancheRepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
             #region
             //ViewData["Titulo"] = "Todos os lanches";    //atribuindo valor na ViewData para ser recuperado na view List 
@@ -22,10 +23,36 @@ namespace VL_VendasLanches.Controllers
             //ViewBag.TotalLanchesCount = lanches.Count();
             #endregion
 
-            var lancheListViewModel = new LancheListViewModel();
-            lancheListViewModel.Lanches = _lancheRepository.Lanches;
-            lancheListViewModel.CategoriaAtual = "Categoria";
-            return View(lancheListViewModel); //enviando para view um IEnumerable de lanches
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
+            
+            if(string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.CategoriaId); 
+                categoriaAtual = "Todos os lacnhes";
+            }else
+            {
+                if(string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase)) //StringComparison.OrdinalIgnoreCase ignora caixa alta ou baixa
+                {
+                    lanches = _lancheRepository.Lanches
+                                .Where(l => l.Categoria.CategoriaNome.ToLower().Equals("Normal".ToLower()))
+                                .OrderBy(l => l.Nome);
+                }else
+                {
+                    lanches = _lancheRepository.Lanches
+                                .Where(l => l.Categoria.CategoriaNome.ToLower().Equals("Natural".ToLower()))
+                                .OrderBy(l => l.Nome);
+                }
+                categoriaAtual = categoria;
+            }
+
+            var lanchesViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            };
+
+            return View(lanchesViewModel); //enviando para view um IEnumerable de lanches
         }
     }
 }
