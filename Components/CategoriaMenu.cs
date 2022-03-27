970d003d3b058b435cc8ddb3a0e6ddc3131383e5
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-03-26 21:13:19 -0300

    Feat: ViewComponent para filtro de produtos no navbar

---
+++ Components/CategoriaMenu.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VL_VendasLanches.Repositories.Interfaces;
+
+namespace VL_VendasLanches.Components
+{
+    [ViewComponent]
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository=categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categorias);
+        }
+    }
+}
